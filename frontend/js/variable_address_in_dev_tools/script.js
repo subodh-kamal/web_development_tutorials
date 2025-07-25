fbe768bd0778d600860a2a39205946ee3a7a1c7b
Subodh Kamal <subodhkamal1621@gmail.com>
2025-07-25 08:39:04 +0545

    add comment

--- frontend/js/variable_address_in_dev_tools/script.js
+++ frontend/js/variable_address_in_dev_tools/script.js
@@ -1,3 +1,9 @@
+/* In short, value is Subodh and refrence is variable address.
+primitive types checks the value and non-primitive type checks the reference.
+In primitve types, if the variable address(reference) is different but the value is same then it will give true result like == true and also === true.
+But in reference types, variable address is checked, so if the variable address(reference) is different, then it will give the result false instead of value being same, like == false and also === false.
+*/
+
 const firstName = "Subodh";
 const firstNameAgain = "Su" + "bodh"; // This string will be assigned a different variable address. But, if we check the equality like == then it will give true and also === true . But in case of non-primitive type == false and === false
 const lastName = "Kamal";
