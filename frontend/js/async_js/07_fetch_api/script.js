60d51fd8dfe0fec1a7e5967ff44149aa270dbdb6
Subodh Kamal <subodhkamal1621@gmail.com>
2025-08-27 21:37:39 +0545

    fetch api

---
+++ frontend/js/async_js/07_fetch_api/script.js
@@ -0,0 +1,10 @@
+fetch("https://dummyjson.com/users")
+  .then((res) => res.json())
+  .then((usersData) => fetch(`https://dummyjson.com/posts/user/${usersData.users[0].id}`))
+  .then((res) => res.json())
+  .then((postsData) => fetch(`https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
+  .then((res) => res.json())
+  .then((commentsData) => fetch(`https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
+  .then((res) => res.json())
+  .then((userData) => console.log(userData))
+  .catch((err) => console.log(err));
