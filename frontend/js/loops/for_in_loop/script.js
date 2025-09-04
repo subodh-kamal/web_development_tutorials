6f3f9e6063f13f7460508479df3818ee8a41fc24
Subodh Kamal <subodhkamal1621@gmail.com>
2025-09-04 10:13:40 +0545

    code addition

--- frontend/js/loops/for_in_loop/script.js
+++ frontend/js/loops/for_in_loop/script.js
@@ -1,9 +1,16 @@
 const person = {
   name: "Subodh",
-  age: 27
+  age: 27,
 };
 
 for (let key in person) {
-  console.log(key);        // "name", "age"
+  console.log(key); // "name", "age"
   console.log(person[key]); // "Subodh", 27
+  console.log(key, ":", person[key]); // "name: Subodh", "age: 27"
 }
+
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
+
+// It is recommendable to not use for...in. Instead, first do Object.keys or Object.values then it will give an array and then use for...of
