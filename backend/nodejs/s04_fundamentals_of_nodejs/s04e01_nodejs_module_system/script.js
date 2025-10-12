79e160ba8c435b45f0d4a1c9a3f0b76055a25064
Subodh Kamal <subodhkamal1621@gmail.com>
2025-10-12 08:25:59 +0545

    code addition

--- backend/nodejs/s04_fundamentals_of_nodejs/s04e01_nodejs_module_system/script.js
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e01_nodejs_module_system/script.js
@@ -1,3 +1,10 @@
 var num = 5;
 
 console.log(num);
+
+// How can we create a global variable in nodejs
+// One way is:
+
+globalThis.num2 = 35; // But this is not a right way to create a global variable.
+
+console.log(num2);
