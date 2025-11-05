b4d981512e78bf6b303d001b5f8b1e1a728b3549
Subodh Kamal <subodhkamal1621@gmail.com>
2025-11-05 08:02:42 +0545

    module wrapper function in node.js

---
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e05_module_wrapper_function/app.js
@@ -0,0 +1,21 @@
+const a = 5;
+console.log(a);
+
+b = 4;
+console.log(b);
+
+function check() {
+    c = 2;
+}
+
+check();
+
+function checkAgain() {
+    const aa = 8;
+}
+
+checkAgain();
+ 
+// a goes to the Local variable section
+
+// b goes to the Global variable section
---
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e05_module_wrapper_function/moduleWrapperFunction.js
@@ -0,0 +1,3 @@
+(function (exports, require, module, __filename, __dirname) {
+    // Module code goes here
+})(exports, require, module, __filename, __dirname);
