0b2d1e996a832d7e9400495b300019a8b3df6d0a
Subodh Kamal <subodhkamal1621@gmail.com>
2025-11-24 08:10:21 +0545

    cli based timer app project

---
+++ backend/nodejs/s03_basics_of_os/s03e11_executable_files/timer_cli_app/app.js
@@ -0,0 +1,8 @@
+console.log("Made with ❤️ by Subodh Kamal");
+console.log("In every second, you are losing your life...");
+console.log("In every 2 to 3 seconds, you are by default losing your breath which is not going to return no matter how much money you spend. So, live your life sensibly.");
+
+let time = 1;
+setInterval(() => {
+  console.log(time++);
+}, 1000);
