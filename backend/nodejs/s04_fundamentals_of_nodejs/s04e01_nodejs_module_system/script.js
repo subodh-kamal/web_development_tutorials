cfad903207ec1299bf4bb422f1844a50ed401ad8
Subodh Kamal <subodhkamal1621@gmail.com>
2025-11-28 04:05:23 +0545

    code correction

--- backend/nodejs/s04_fundamentals_of_nodejs/s04e01_nodejs_module_system/script.js
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e01_nodejs_module_system/script.js
@@ -1,10 +1,10 @@
-var num = 5;
+var num = 5; // This variable is available in Local Scope.
 
 console.log(num);
 
-// How can we create a global variable in nodejs
+// How can we create a Global variable in Node.js
 // One way is:
 
-globalThis.num2 = 35; // But this is not a right way to create a global variable.
+globalThis.num2 = 35; // But, this is not a right way to create a Global variable.
 
 console.log(num2);
