24cc038802119fd7e9944694871dc5972c7ca42c
Subodh Kamal <subodhkamal1621@gmail.com>
2025-12-02 09:19:40 +0545

    code correction and code addition

--- backend/nodejs/s03_basics_of_os/s03e04_thread/app.js
+++ backend/nodejs/s03_basics_of_os/s03e04_thread/app.js
@@ -31,3 +31,41 @@ new Worker("./c"); // Third thread
 // Concept of 'Concurrency' and 'Parallelism'
 
 // Parallelism means running different process at the same time in different cores for the full time.
+
+
+
+
+
+/* 
+
+Concepts/Jargons in OS
+
+- What is OS
+- What is Kernel
+- What is CPU
+- What is Cores - Physical cores and Logical cores
+- What is Process
+- Spawning a process
+- States of process -> Ready, Running, Waiting, Terminated, Sleep
+- What is Thread
+- Can a process exist without a thread
+- Context Switching
+- Concurency vs Parallelism\
+- Memory (RAM)
+- Storage (ROM)
+- Environment Variables
+  - In Windows OS:
+    - 1. User-defined Variables (Permanent)
+    - 2. System Variables (Permanent)
+    - 3. Process Variables (Temporary)
+
+- Files, Folders, Symbolic Links, and Path Systems
+
+- Executable Files:
+    - Script executable file (any script file that executes the code inside it, like .sh, .js, .css, .html, etc.)
+    - Binary executable file (having .exe extension in Windows OS, but in Linux, it is extensionless )
+
+- File Permissions in Windows and Linux
+- How Commands work in Terminal (5 Categories)
+
+*/
