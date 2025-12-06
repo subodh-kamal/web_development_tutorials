9aed06badd00a28611139e0396e58e7addc34fe9
Subodh Kamal <subodhkamal1621@gmail.com>
2025-12-06 13:13:37 +0545

    shebang

---
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e15_shebang/app.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log("This output is generating from app.js file");
+
+// let a = 0;
+
+// setInterval(() => {
+//   console.log(++a);
+// }, 1000);
+
+
+// Question at timestamp: 20:54 | To solve: use fs module and use the concept of os that you've learned. And use ChatGPT if needed
