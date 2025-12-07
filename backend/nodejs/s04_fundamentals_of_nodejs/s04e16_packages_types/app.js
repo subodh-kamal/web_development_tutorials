7b631dd54b4ee076fbb1e817eba026cd6111e535
Subodh Kamal <subodhkamal1621@gmail.com>
2025-12-07 20:07:21 +0545

    code correction

--- backend/nodejs/s04_fundamentals_of_nodejs/s04e16_packages_types/app.js
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e16_packages_types/app.js
@@ -1,11 +1,11 @@
 /* 
-- Library package -> axios
-- CLI package -> vite | starts with the help of npx | We usually install    these packages as devDependencies. (development dependencies)
+- 1. Library package -> axios
+- 2. CLI package -> vite | starts with the help of npx | We usually install    these packages as devDependencies(development dependencies). Example of CLI packages: vite, tsc(typescript compiler), cra(create-react-app), etc.
 - Generally, Library packages belongs to dependencies and CLI packages belongs to devDependencies but not all the time.
 
-- LOCAL PACKAGES: Those packages which are installed inside node_modules folder are called local packages.
+- 3. LOCAL PACKAGES: Those packages which are installed inside node_modules folder are called local packages.
 
-- GLOBAL PACKAGES: -g flag | If we install any package as global package then we do not need to use npx command. We can directly use the package name. Like: vite
+- 4. GLOBAL PACKAGES: -g flag | If we install any package as global package then we do not need to use npx command. We can directly use the package name. Like: vite
 
 */
 
