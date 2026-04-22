12e55e01abab4c86c35c9187ac538b643c75cb84
Subodh Kamal <subodhkamal1621@gmail.com>
2026-04-22 08:15:02 +0545

    code correction and addition

--- backend/nodejs/s04_fundamentals_of_nodejs/s04e13_our_own_npm_package/app.js
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e13_our_own_npm_package/app.js
@@ -1,4 +1,5 @@
 // import { num } from "maths"; // ES6 Module System
-const {num} = require("maths") // CJS Module System
+const {num, dummy} = require("maths") // CJS Module System
 
 console.log(num);
+console.log(dummy);
