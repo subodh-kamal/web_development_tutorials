5080505bf4ee1568d327c9cadbfcfe5abe54b5ec
Subodh Kamal <subodhkamal1621@gmail.com>
2026-07-23 23:02:17 +0545

    code correction and addition

--- backend/nodejs/s07_event_driven_architecture_and_event_emitter/s07e02_async_io_in_nodejs/app.js
+++ backend/nodejs/s07_event_driven_architecture_and_event_emitter/s07e02_async_io_in_nodejs/app.js
@@ -1,11 +1,23 @@
-import fsPromises from "fs/promises";
+// import fsPromises from "node:fs/promises";
+import fs from "node:fs";
 
 setTimeout(() => {
   console.log("hello");
 }, 0);
 
-// Async I/O
-const fileContent = await fsPromises.readFile("async_io.md", "utf-8");
+// // Async I/O
+// const fileContent = await fsPromises.readFile("async_io.md", "utf-8");
+// console.log(fileContent);
+
+// Sync I/O
+const fileContent = fs.readFileSync("a.txt", "utf-8");
 console.log(fileContent);
 
 console.log("LAST LINE");
+
+
+// Synchronous code
+// Asynchronous code
+// Promisified code
+// .then, .catch, .finally
+// async/await
