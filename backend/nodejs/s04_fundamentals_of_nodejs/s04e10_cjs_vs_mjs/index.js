f6e0604a853c95da641ffa01275d8a1fb7e12f9b
Subodh Kamal <subodhkamal1621@gmail.com>
2026-08-14 21:16:58 +0545

    code correction and addition

--- backend/nodejs/s04_fundamentals_of_nodejs/s04e10_cjs_vs_mjs/index.js
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e10_cjs_vs_mjs/index.js
@@ -1,15 +1,22 @@
 // const timer = require('./timer.js');
 
-console.log("Rnning index.js");
-import timer from "./timer.js";
-console.log(timer);
-
+// console.log("Rnning index.js");
+// import timer from "./timer.js";
+// console.log(timer);
 
 // import timer from "./timer.js"; // ES6 modules are hoisted.
 
 // This is how we use top level await in ES6 only. We cannot use like this in CJS
-// const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-// console.log(response);
+
+async function check() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/4");
+  const result = await response.json();
+
+  console.log(result);
+  console.log("I am a Synchronous code!");
+}
+check();
+console.log("hello");
 
 // This is how we use await in CJS but it is not common in use. So avoid it
 // (async () => {
