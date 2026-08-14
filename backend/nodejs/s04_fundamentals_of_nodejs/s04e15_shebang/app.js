03555951e0f55be0ea058d5c6435fa44773a0745
Subodh Kamal <subodhkamal1621@gmail.com>
2026-08-14 21:20:56 +0545

    code correction and addition

--- backend/nodejs/s04_fundamentals_of_nodejs/s04e15_shebang/app.js
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e15_shebang/app.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
+
+// Now, just go to the terminal and hit ./app.js and boom app.js file starts executing by using shebang.
+
 console.log("This output is generating from app.js file");
 
-// let a = 0;
+let a = 0;
 
-// setInterval(() => {
-//   console.log(++a);
-// }, 1000);
-
+setInterval(() => {
+  console.log(++a);
+}, 1000);
 
 // Question at timestamp: 20:54 | To solve: use fs module and use the concept of os that you've learned. And use ChatGPT if needed
