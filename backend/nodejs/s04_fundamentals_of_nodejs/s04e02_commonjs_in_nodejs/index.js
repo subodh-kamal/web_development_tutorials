884b80c547b3e0209daabb055c789ad131d8ad1b
Subodh Kamal <subodhkamal1621@gmail.com>
2026-08-14 21:12:24 +0545

    code correction and addition

--- backend/nodejs/s04_fundamentals_of_nodejs/s04e02_commonjs_in_nodejs/index.js
+++ backend/nodejs/s04_fundamentals_of_nodejs/s04e02_commonjs_in_nodejs/index.js
@@ -1,5 +1,14 @@
-// Each file is called a module in node.js And there are 2 types of module systems in Node.js For eg: Common js module system(CJS), ES6 module system (ESM) 
+// Each file is called a module in node.js And there are 2 types of module systems in Node.js i.e., Common js module system(CJS), ES6 module system (ESM) 
 // The return value of require function is module.exports and math.exports is an object.
+
+/*  *** IMPORTANT *** -> require function works in 3 steps:
+1. Finds the file that is provided to the require function like: require("./sum")
+2. Executes all the code except module.exports inside the provided file
+3. Executes the code module.exports
+
+####### reqire is a synchronous function
+
+*/
 
 // console.log(typeof require); // output: function
 
@@ -10,13 +19,13 @@ console.log(product(1, 2, 3, 4));
 
 // console.log(sum);
 
-/* const sum = require("./sum"); // This require function takes only module(file) as an argument. Nothing else will work here.
-const product = require("./product")
+// const sum = require("./sum"); // This require function takes only module(file) as an argument. Nothing else will work here other than valid file path.
+// const product = require("./product")
 
-console.log(sum(1, 2, 3, 4));
+/* console.log(sum(1, 2, 3, 4));
 console.log(product(1, 2, 3, 4));
 
-console.log(typeof require); // function
-console.log(typeof module); // object
 console.log(typeof exports); // object
-console.log(typeof module.exports); // object */
+console.log(typeof module); // object
+console.log(typeof module.exports); // object 
+console.log(typeof require); // function */
